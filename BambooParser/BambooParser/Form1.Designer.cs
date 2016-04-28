90c71eb8f8ba448c66e1eab366d46c3ba8c59db2
Michael Mueller <mmuell@gmail.com>
2016-04-28 17:11:03 -0500

    Initial Commit

---
+++ BambooParser/BambooParser/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BambooParser
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogFileST = new System.Windows.Forms.Label();
+            this.ParsePB = new System.Windows.Forms.Button();
+            this.LogFileET = new System.Windows.Forms.TextBox();
+            this.ResultsET = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LogFileST
+            // 
+            this.LogFileST.AutoSize = true;
+            this.LogFileST.Location = new System.Drawing.Point(12, 18);
+            this.LogFileST.Name = "LogFileST";
+            this.LogFileST.Size = new System.Drawing.Size(61, 20);
+            this.LogFileST.TabIndex = 0;
+            this.LogFileST.Text = "LogFile";
+            // 
+            // ParsePB
+            // 
+            this.ParsePB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ParsePB.Location = new System.Drawing.Point(954, 8);
+            this.ParsePB.Name = "ParsePB";
+            this.ParsePB.Size = new System.Drawing.Size(75, 41);
+            this.ParsePB.TabIndex = 1;
+            this.ParsePB.Text = "Parse";
+            this.ParsePB.UseVisualStyleBackColor = true;
+            this.ParsePB.Click += new System.EventHandler(this.ParsePB_Click);
+            // 
+            // LogFileET
+            // 
+            this.LogFileET.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogFileET.Location = new System.Drawing.Point(79, 15);
+            this.LogFileET.Name = "LogFileET";
+            this.LogFileET.Size = new System.Drawing.Size(869, 26);
+            this.LogFileET.TabIndex = 2;
+            this.LogFileET.Text = "C:\\Users\\mmuel_000\\Dropbox\\Scripts and Notes\\2016\\04 - April\\ASC-LCC-BLD-6455.log" +
+    "";
+            // 
+            // ResultsET
+            // 
+            this.ResultsET.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultsET.Location = new System.Drawing.Point(16, 59);
+            this.ResultsET.Multiline = true;
+            this.ResultsET.Name = "ResultsET";
+            this.ResultsET.ReadOnly = true;
+            this.ResultsET.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ResultsET.Size = new System.Drawing.Size(1013, 380);
+            this.ResultsET.TabIndex = 3;
+            this.ResultsET.WordWrap = false;
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 451);
+            this.Controls.Add(this.ResultsET);
+            this.Controls.Add(this.LogFileET);
+            this.Controls.Add(this.ParsePB);
+            this.Controls.Add(this.LogFileST);
+            this.Name = "MainView";
+            this.Text = "Bamboo Log Parsee";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LogFileST;
+        private System.Windows.Forms.Button ParsePB;
+        private System.Windows.Forms.TextBox LogFileET;
+        private System.Windows.Forms.TextBox ResultsET;
+    }
+}
+
---
+++ BambooParser/BambooParser/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BambooParser
+{
+    public partial class MainView : Form
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void ParsePB_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(LogFileET.Text))
+            {
+                return;
+            }
+            LogParser parser = new LogParser(LogFileET.Text, SetText);
+            parser.Parse();
+        }
+
+        private void SetText(string text)
+        {
+            if (ResultsET.InvokeRequired)
+            {
+                ResultsET.Invoke((MethodInvoker) (() => SetText(text)));
+                return;
+            }
+            ResultsET.AppendText(Environment.NewLine);
+            ResultsET.AppendText(text);
+        }
+    }
+
+    internal class LogParser
+    {
+        private readonly string _fileName;
+        private readonly Action<string> _writeText;
+
+        public LogParser(string fileName, Action<string> writeText)
+        {
+            _fileName = fileName;
+            _writeText = writeText;
+        }
+
+        public void Parse()
+        {
+            Thread t1 = new Thread(ParseImpl);
+            t1.Start();
+        }
+
+        private void ParseImpl()
+        {
+            if (!File.Exists(_fileName))
+            {
+                _writeText(string.Format("File {0} was not found", _fileName));
+            }
+            DateTime? lastDate = null;
+            string lastLine = null;
+            foreach (var line in File.ReadAllLines(_fileName))
+            {
+                try
+                {
+                    var parsed = line.Split('\t');
+                    var date = DateTime.Parse(parsed[1]);
+                    if (lastDate == null)
+                    {
+                        lastDate = date;
+                    }
+                    var sinceLast = date - lastDate.Value;
+                    if (sinceLast.TotalSeconds > 9)
+                    {
+                        _writeText(lastLine);
+                        _writeText(string.Format("Took {0} MS", sinceLast.TotalMilliseconds));
+                    }
+                    lastLine = line;
+                    lastDate = date;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
